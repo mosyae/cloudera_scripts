cfe2a014b9695d8223495999628f1e57931d5834
mosyae <33257923+mosyae@users.noreply.github.com>
2018-11-25 15:53:37 +0200

    Update disbale_ipv6.sh

--- disbale_ipv6.sh
+++ disbale_ipv6.sh
@@ -1,14 +1,14 @@
 if grep -q net.ipv6.conf.all.disable_ipv6 /etc/sysctl.conf; then
-    sed -i '/net.ipv6.conf.all.disable_ipv6/c\net.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.conf
+    sudo sed -i '/net.ipv6.conf.all.disable_ipv6/c\net.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.conf
 else
-    echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
+    sudo sed -e "\$anet.ipv6.conf.all.disable_ipv6 = 1" /etc/sysctl.conf
 fi
 
 if grep -q net.ipv6.conf.default.disable_ipv6 /etc/sysctl.conf; then
-    sed -i '/net.ipv6.conf.default.disable_ipv6/c\net.ipv6.conf.default.disable_ipv6 = 1' /etc/sysctl.conf
+    sudo sed -i '/net.ipv6.conf.default.disable_ipv6/c\net.ipv6.conf.default.disable_ipv6 = 1' /etc/sysctl.conf
 else
-    echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
+    sudo sed -e "\$anet.ipv6.conf.default.disable_ipv6 = 1" /etc/sysctl.conf
 fi
 
-sysctl -p
+sudo sysctl -p
 # ip addr show | grep net6
